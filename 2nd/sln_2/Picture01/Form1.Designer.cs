60acb90357cfcd6f80bee557462a11bf7e697606
KimTaeWook <xodnr2019@naver.com>
2023-09-12 12:43:12 +0900

    0912

---
+++ 2nd/sln_2/Picture01/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace Picture01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClickThis = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnClickThis
+            // 
+            this.btnClickThis.BackColor = System.Drawing.SystemColors.Info;
+            this.btnClickThis.Font = new System.Drawing.Font("돋움", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnClickThis.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnClickThis.Location = new System.Drawing.Point(250, 174);
+            this.btnClickThis.Name = "btnClickThis";
+            this.btnClickThis.Size = new System.Drawing.Size(161, 68);
+            this.btnClickThis.TabIndex = 0;
+            this.btnClickThis.Text = "화면 초기화";
+            this.btnClickThis.UseVisualStyleBackColor = false;
+            this.btnClickThis.Click += new System.EventHandler(this.btnClickThis_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClickThis);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "간단한 그림판";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClickThis;
+    }
+}
+
---
+++ 2nd/sln_2/Picture01/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Picture01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            // 초기화 구성 - 왜 초기화 시킬까 ? = 메모리 공간의 확보 - 컴파일러 한테 알려주는 것.
+            InitializeComponent();
+        }
+
+        int x1, y1, x2, y2;
+
+        private void btnClickThis_Click_1(object sender, EventArgs e)
+        {
+            this.Refresh();
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            // 끝 지점 설정
+            x2 = e.X;
+            y2 = e.Y;
+
+            // 시작점과 끝 지점을 설정을 했으면 마우스 업 할때 그려주어라
+
+            
+            Graphics g = CreateGraphics(); // 그래픽을 생성해준다
+            Pen pen = new Pen(Color.Blue, 10);
+            g.DrawLine(pen, x1, y1, x2, y2); // 변수 g는 델리게이트 대리자 (데이터 형)이다. - 그래서 함수를 담을 수 있는 것이다,
+        }
+
+       private void Form1_MouseDown(object sender, MouseEventArgs e)
+       {
+            // 시작점 설정
+            x1 = e.X;
+            y1 = e.Y;
+       }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
